663a0048179e68ef5f2ad4c95b7905a97bb970b1
Dallana Cardoza Martinez <cardozadallana16@gmail.com>
2023-03-01 09:27:22 -0600

    Lista de Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import './App.css'
+
+function App() {
+  const nombre = "Dallana Cardoza Martinez"
+
+  return (<div>
+    <h3>Lista de Hobbies de {nombre} </h3>
+ 
+    <ol>
+      <li> Jugar </li>
+      <li> Hacer deporte </li>
+      <li> Hacer deporte </li>
+      <li> Ver peliculas </li>
+
+
+    </ol>
+    </div>)
+  
+}
+
+export default App
